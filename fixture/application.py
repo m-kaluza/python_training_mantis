3b0ee471c4a7eb15b51d1af44f92d5262fd2c8f6
m-kaluza <orchid@vp.pl>
2020-03-25 21:39:18 +0100

    telnet

--- fixture/application.py
+++ fixture/application.py
@@ -5,7 +5,7 @@ from fixture.james import JamesHelper
 
 class Application:
 
-    def __init__(self, browser, base_url):
+    def __init__(self, browser, config):
         if browser == "firefox":
             self.wd = webdriver.Firefox()
         elif browser == "chrome":
@@ -17,7 +17,8 @@ class Application:
         self.session = SessionHelper(self)
         self.james = JamesHelper(self)
         self.accept_next_alert = True
-        self.base_url = base_url
+        self.config = config
+        self.base_url = config['web']['baseUrl']
 
     def is_valid(self):
         try:
